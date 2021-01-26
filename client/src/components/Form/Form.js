5b8f1b52fa34544c3b449156934b4434a146a758
Suraj Hanvate <surajhanvate999@gmail.com>
2021-01-26 17:07:09 +0530

    Updating Posts

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import { useDispatch, useSelector } from "react-redux";
+
+import useStyles from "./styles";
+import { createPost, updatePost } from "../../actions/posts";
+
+const Form = ({ currentId, setCurrentId }) => {
+    const [postData, setPostData] = useState({
+        creator: "",
+        title: "",
+        message: "",
+        tags: "",
+        selectedFile: "",
+    });
+    const post = useSelector((state) =>
+        currentId ? state.posts.find((p) => p._id === currentId) : null
+    );
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (post) setPostData(post);
+    }, [post]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (currentId) {
+            dispatch(updatePost(currentId, postData));
+        }
+        dispatch(createPost(postData));
+    };
+
+    const clear = () => {};
+
+    return (
+        <Paper className={classes.paper}>
+            <form
+                autoComplete="off"
+                noValidate
+                className={`${classes.root} ${classes.form}`}
+                onSubmit={handleSubmit}
+            >
+                <Typography variant="h6">Creating a Memory</Typography>
+                <TextField
+                    name="creator"
+                    variat="outlined"
+                    label="Creator"
+                    fullWidth
+                    value={postData.creator}
+                    onChange={(e) =>
+                        setPostData({ ...postData, creator: e.target.value })
+                    }
+                ></TextField>
+                <TextField
+                    name="title"
+                    variat="outlined"
+                    label="Title"
+                    fullWidth
+                    value={postData.title}
+                    onChange={(e) =>
+                        setPostData({ ...postData, title: e.target.value })
+                    }
+                ></TextField>
+                <TextField
+                    name="message"
+                    variat="outlined"
+                    label="Message"
+                    fullWidth
+                    value={postData.message}
+                    onChange={(e) =>
+                        setPostData({ ...postData, message: e.target.value })
+                    }
+                ></TextField>
+                <TextField
+                    name="tags"
+                    variat="outlined"
+                    label="Tags"
+                    fullWidth
+                    value={postData.tags}
+                    onChange={(e) =>
+                        setPostData({ ...postData, tags: e.target.value })
+                    }
+                ></TextField>
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type="file"
+                        multiple={false}
+                        onDone={({ base64 }) =>
+                            setPostData({ ...postData, selectedFile: base64 })
+                        }
+                    />
+                </div>
+                <Button
+                    className={classes.buttonSubmit}
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    type="submit"
+                    fullWidth
+                >
+                    Submit
+                </Button>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={clear}
+                    size="small"
+                    fullWidth
+                >
+                    Clear
+                </Button>
+            </form>
+        </Paper>
+    );
+};
+
+export default Form;
